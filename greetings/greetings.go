a3e8cff1942a93ea1d846537a4b15c3a97973bbf
Jonathan <jonathan.github@gmail.com>
2021-07-01 20:12:48 -0300

    returning and handling errors

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,11 +1,19 @@
 // declaring the package to collect related functions
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // A function with capitalized letter can be called by a
 // function not in the same module
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	// the := operator a is shortcut for declaring and
 	// initialing a variable in one line (Go uses the
 	// value on the right to determine the variableś type).
@@ -13,5 +21,5 @@ func Hello(name string) string {
 	// var message string
 	// message = fmt.Sprintf("Hi, %v. Welcome!", name)
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 	"rsc.io/quote"
@@ -11,7 +12,19 @@ func main() {
 	fmt.Println("using quote package")
 	fmt.Println(quote.Go())
 
-	message := greetings.Hello("Gladys")
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("Gladys")
+	// If an error was returned, print it to the console and exit the program
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
 	fmt.Println("using greetings package")
 	fmt.Println(message)
 }
