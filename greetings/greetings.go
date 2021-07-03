7dd54a24382736d5557c0f8004f4c91aab94f194
Jonathan <jonathan.github@gmail.com>
2021-07-03 14:01:34 -0300

    returning a random greeting using slice and random index.

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -4,7 +4,28 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
+
+// init sets initial values for variables used in the function
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!"}
+
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
+}
 
 // A function with capitalized letter can be called by a
 // function not in the same module
@@ -20,6 +41,6 @@ func Hello(name string) (string, error) {
 	// long way:
 	// var message string
 	// message = fmt.Sprintf("Hi, %v. Welcome!", name)
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
 }
